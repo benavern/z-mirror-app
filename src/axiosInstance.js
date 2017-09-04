46848a1650e303893062d597708d4b56359e8657
Benjamin Caradeuc <bencrdc@gmail.com>
2017-09-05 00:42:14 +0200

    added token simple security

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -2,5 +2,6 @@ import axios from 'axios'
 import config from './config'
 
 export default axios.create({
-  baseURL: config.api.url
+  baseURL: config.api.url,
+  headers: {'X-Z_MIRROR_TOKEN': config.api.token}
 })
--- src/config.example.js
+++ src/config.example.js
@@ -1,6 +1,7 @@
 export default {
   api: {
     // https://github.com/benavern/z-mirror-api
-    url: '[[URL_OF_Z_MIRROR_API]]'
+    url: '[[URL_OF_Z_MIRROR_API]]',
+    token: '[[TOKEN_OF_Z_MIRROR_API]]'
   }
 }
